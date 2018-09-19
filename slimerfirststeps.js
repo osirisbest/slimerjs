12f73583e279c4f383b16e8ce3016102ef58e3a8
osirisbest <osirisbest@gmail.com>
2018-09-19 20:59:21 +0300

    json

--- slimerfirststeps.js
+++ slimerfirststeps.js
@@ -1,13 +1,15 @@
-let url='https://eva.ua/024-104-225/brnd-73044/pr20594-shampun-pantene-pro-v/'
+const url='https://eva.ua/024-104-225/brnd-73044/pr20594-shampun-pantene-pro-v/'
 
-var page = require('webpage').create();
+let page = require('webpage').create();
 page.open(url, function (status) {
     
-    var mainTitle = page.evaluate(function () {
+    var content = page.evaluate(function () {
         var selector=".base"
         console.log('message from the web page');
-        return document.querySelector(selector).textContent;
+        return {content:document.querySelector(selector).textContent,
+            selector
+        };
     });
-    console.log(`The value of ${selector} is ${mainTitle}`);
+    console.log(`The value is ${JSON.stringify( content)}`);
     slimer.exit()
 });

f663652f127c807ba1f9004925bda99ea1713669
osirisbest <osirisbest@gmail.com>
2018-09-20 15:04:24 +0300

    pause

--- slimerfirststeps.js
+++ slimerfirststeps.js
@@ -1,13 +1,42 @@
 const url='https://eva.ua/024-104-225/brnd-73044/pr20594-shampun-pantene-pro-v/'
 
 let page = require('webpage').create();
-page.open(url, function (status) {
+//callback for console from page
+page.onConsoleMessage = function (msg) {
+    console.log(msg);
+};
+
+page.open(url)
+//slimer.wait(1000)
+.then( function (status) {
+    page.viewportSize = { width:1024, height:768 };
+    // var content = page.evaluate (function () {
+     
+    //     var selector=`.swatch-option`
+    //     console.log('message from the web page');
+    //     return  {content:document.querySelector(selector).textContent, selector}
+    //                                          }
+
+                               // );
+    //console.log(`The value is ${JSON.stringify( content)}`);
+    slimer.wait(2000)
+    var val=page.evaluate(function(){
+        //var evt = document.createEvent("MouseEvents")
+        //evt.initMouseEvent("click", true, true, window,
+        //0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        //slimer.wait(1000)
+        let selectors=document.querySelector('.swatch-option')
+        // console.log(selectors)
+        //setTimeout(()=>{
+        selectors.click()
+        console.log('click')
+        //}
+        //,2000)
+        
+        //return selectors.dispatchEvent(evt)
+    }
     
-    var content = page.evaluate(function () {
-        var selector=".base"
-        console.log('message from the web page');
-        return  {content:document.querySelector(selector).textContent, selector
-                }                            });
-    console.log(`The value is ${JSON.stringify( content)}`);
+    )
+    slimer.wait(5000)
     slimer.exit()
 });
